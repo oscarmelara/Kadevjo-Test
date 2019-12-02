6f092b43baa33b3cd8c16d215f95fa1d60a2d4f5
oscarmelara <melara2396@gmail.com>
2019-12-02 17:24:44 -0600

    :sparkles: testing deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/css/swiper.css' }
     ]
   },
   /*
